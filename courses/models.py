12f9adac1f9363b1b70db77ecc81e2e6b4ddfd12
fauziwig <triagungwiguna@gmail.com>
2025-11-03 21:08:48 +0700

    tambah model QuizAttempt dan QuizAnswer

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Material(models.Model):
     title = models.CharField(max_length=200)
@@ -25,3 +26,32 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class QuizAttempt(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='quiz_attempts')
+    material = models.ForeignKey(Material, on_delete=models.CASCADE, related_name='attempts')
+    score = models.IntegerField()
+    total_questions = models.IntegerField()
+    passed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-completed_at']
+
+    def __str__(self):
+        return f"{self.user.username} - {self.material.title} - {self.score}/{self.total_questions}"
+
+    @property
+    def percentage(self):
+        if self.total_questions > 0:
+            return round((self.score / self.total_questions) * 100, 2)
+        return 0
+
+class QuizAnswer(models.Model):
+    attempt = models.ForeignKey(QuizAttempt, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE, null=True, blank=True)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.attempt.user.username} - {self.question.question_text[:50]}"
